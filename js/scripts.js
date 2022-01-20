ab9c4ac3066af282f612265030c8f9e7c9ba45d4
Aaron Boyd <a.boyd91@yahoo.com>
2022-01-19 19:30:57 -0800

    create prompt asking users to enter 2 numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ function add(number1, number2)  {
   return number1 + number2;
 }
 
-const number1 = prompt("Enter a number:");
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
 
-alert(number1);
+alert(add(number1, number2));
